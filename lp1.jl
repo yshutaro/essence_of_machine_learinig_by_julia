8bd0c41901343404c8dbce9d140a680b414116a3
yshutaro <y.shutaro@gmail.com>
2019-01-25 23:51:30 +0900

    04-08数理最適化-線形計画問題

---
+++ lp1.jl
@@ -0,0 +1,18 @@
+using JuMP
+using Clp
+
+m = Model(solver=ClpSolver())
+@variable(m, x >= 0)
+@variable(m, y >= 0)
+
+@objective(m, Max, 3 * x + 4 * y )
+
+@constraint(m, x + 4 * y <= 1700)
+@constraint(m, 2 * x + 3 * y <= 1400)
+@constraint(m, 2 * x + y <= 1000)
+
+status = solve(m)
+
+println(getvalue(x))
+println(getvalue(y))
+println(getobjectivevalue(m))
