ca9ed15e02de99f79f53846f3321efb23b6b57e6
yshutaro <y.shutaro@gmail.com>
2019-01-30 23:21:32 +0900

    04-08数理最適化　勾配降下法 α=0.01とグラフ表示まで

--- gd.jl
+++ gd.jl
@@ -1,28 +1,33 @@
 module gd
 
 mutable struct GradientDescent
-  path_::Array
+  path_x_::Array
+  path_y_::Array
   x_::Array
   opt_::Float64
 
   function GradientDescent()
-    new([], [], 0.0)
+    new([], [], [], 0.0)
   end
 end
 
 function solveGradientDescent(g::GradientDescent, f, df, init::Array, α=0.01, ϵ=1e-6)
   x = init
-  path = []
+  path_x = []
+  path_y = []
   grad = df(x)
-  append!(path, x)
+  append!(path_x, x[1])
+  append!(path_y, x[2])
   
   while (sum(grad.^2)) > ϵ^2
     x = x - α * grad
     grad = df(x)
-    append!(path, x)
+    append!(path_x, x[1])
+    append!(path_y, x[2])
   end
 
-  g.path_ = path
+  g.path_x_ = path_x
+  g.path_y_ = path_y
   g.x_ = x
   g.opt_ = f(x)
 end
--- gd_test1.jl
+++ gd_test1.jl
@@ -19,3 +19,15 @@ gd.solveGradientDescent(algo, f, df, initial)
 
 println(algo.x_)
 println(algo.opt_)
+
+plt1 = scatter(algo.path_x_, algo.path_y_, color="black", markersize=1)
+
+xs = LinRange(-2, 2, 300)
+ys = LinRange(-2, 2, 300)
+
+h(x,y) = f([x, y]) # 配列の形に直す
+zs = h.(xs', ys)
+levels = [-3, -2.9, -2.8, -2.6, -2.4, -2.2, -2, -1, -1, 0, 1, 2, 3, 4]
+
+plt2 = contour(zs, levels=levels, color="black", linestyle=:dash)
+plot(plt1, plt2)
