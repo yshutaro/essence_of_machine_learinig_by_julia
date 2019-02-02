99c4349c63d0db8961d99bef01fb06306ddeb3f6
yshutaro <y.shutaro@gmail.com>
2019-02-03 02:43:39 +0900

    04-08数理最適化　ニュートン法完成

--- newton_nograph.jl
+++ newton_nograph.jl
@@ -1,13 +1,11 @@
 module newton
 
-mutable struct Newton
+struct Newton
   ϵ::Float64
   max_iter::Int
-  path_x_::Array
-  path_y_::Array
 
   function Newton(ϵ=1e-10, max_iter=1000)
-    new(ϵ, max_iter, [], [])
+    new(ϵ, max_iter)
   end
 end
 
@@ -15,12 +13,8 @@ function solveNewton(newton::Newton, f, df, x0)
   x = x0
   x_new = 0
   iter = 0
-  push!(newton.path_x_, x0[1])
-  push!(newton.path_y_, x0[2])
   while true
     x_new = x - inv(df(x)) * f(x)
-    push!(newton.path_x_, x_new[1])
-    push!(newton.path_y_, x_new[2])
     if sum((x - x_new).^2) < (newton.ϵ)^2
       break
     end
@@ -34,3 +28,4 @@ function solveNewton(newton::Newton, f, df, x0)
 end
 
 end
+
--- newton_test1.jl
+++ newton_test1.jl
@@ -1,4 +1,5 @@
 using .newton
+using Plots
 
 f1(x, y) = x^3 - 2*y
 f2(x, y) = x^2 + y^2 -1
@@ -15,9 +16,30 @@ function df(xx)
   return [3*x^2 -2; 2*x 2*y]
 end
 
+xmin = -3
+xmax = 3
+ymin = -3
+ymax = 3
+
+x = LinRange(xmin, xmax, 200)
+y = LinRange(ymin, ymax, 200)
+
+z1 = f1.(x',y)
+z2 = f2.(x',y)
+
 solver = newton.Newton()
 initials = [[1,1],[-1,-1],[1,-1]]
+solvers = []
 for x0 in initials
   sol = newton.solveNewton(solver, f, df, x0)
+  push!(solvers, solver)
   println(sol)
 end
+
+contour(x, y, z1, color="red", levels=[0], xlim=(xmin, xmax), ylim=(ymin, ymax))
+contour!(x, y, z2, color="black", levels=[0])
+
+scatter!(solvers[1].path_x_, solvers[1].path_y_, color="black", markershape=:+)
+scatter!(solvers[2].path_x_, solvers[2].path_y_, color="black", markershape=:star8)
+scatter!(solvers[3].path_x_, solvers[3].path_y_, color="black", markershape=:x)
+
