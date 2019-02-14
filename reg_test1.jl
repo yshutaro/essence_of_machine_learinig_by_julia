5e60527758ed5c7569d55a293f44f885ef41064f
yshutaro <y.shutaro@gmail.com>
2019-02-14 22:25:28 +0900

    05-02回帰　グラフ追加途中

--- reg_test1.jl
+++ reg_test1.jl
@@ -15,3 +15,11 @@ model = linearreg.LinearRegression()
 linearreg.fit(model, X, y)
 println("係数:", model.w_)
 println("(1, 1)に対する予測値:", linearreg.predict(model, [1,1]))
+
+x = LinRange(0, 20, scale)
+y = LinRange(0, 20, scale)
+xmesh = repeat(x', outer=(length(y),1))
+ymesh = repeat(y,  outer=(1,length(x)))
+zmesh = reshape(model.w_[1] .+ model.w_[2] .+ vec(xmesh) .+ model.w_[3] .* vec(ymesh), size(xmesh))
+
+plot(xmesh, ymesh, zmesh, color="black", st=:wireframe)
