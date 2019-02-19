67de6f2d251e06f6bdca77d88b481d40b5ba87c1
yshutaro <y.shutaro@gmail.com>
2019-02-20 00:31:33 +0900

    05-03リッジ回帰 後半途中2

--- ridge_test2.jl
+++ ridge_test2.jl
@@ -16,12 +16,23 @@ ymax = 25
 xx = x[1:2 + 0 * 2]
 yy = x[1:2 + 0 * 2]
 
-scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
+plt1 = scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
 
 model = linearreg.LinearRegression()
 linearreg.fit(model, xx, yy)
 xs = [xmin, xmax]
 ys = [model.w_[1] + model.w_[2] * xmin,
 model.w_[1] + model.w_[2] * xmax]
-plot!(xs, ys, color="black")
+plt2 = plot!(plt1, xs, ys, color="black")
+
+model = ridge.RidgeRegression(10.)
+ridge.fit(model, xx,yy)
+xs = [xmin, xmax]
+ys = [model.w_[1] + model.w_[2] * xmin,
+model.w_[1] + model.w_[2] * xmax]
+
+plt3 = scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
+plt4 = plot!(plt3, xs, ys, color="black")
+
+plot(plt2, plt4, laout=(2,1))
 
