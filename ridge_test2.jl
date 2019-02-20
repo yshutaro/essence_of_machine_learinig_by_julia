eed2e49e2c136e58cf750ce0577b13686fd501e1
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-02-20 10:25:10 +0900

    for文を追加してそれぞれ5つ図を表示するようにした

--- ridge_test2.jl
+++ ridge_test2.jl
@@ -12,27 +12,33 @@ xmax = 12
 ymin = -1
 ymax = 25
 
+plt_l = []
+plt_r = []
 
-xx = x[1:2 + 0 * 2]
-yy = x[1:2 + 0 * 2]
+for i in 0:4
+    xx = x[1:2 + i * 2]
+    yy = x[1:2 + i * 2]
+    plt_s1= scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
 
-plt1 = scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
+    model = linearreg.LinearRegression()
+    linearreg.fit(model, xx, yy)
+    xs = [xmin, xmax]
+    ys = [model.w_[1] + model.w_[2] * xmin,
+    model.w_[1] + model.w_[2] * xmax]
 
-model = linearreg.LinearRegression()
-linearreg.fit(model, xx, yy)
-xs = [xmin, xmax]
-ys = [model.w_[1] + model.w_[2] * xmin,
-model.w_[1] + model.w_[2] * xmax]
-plt2 = plot!(plt1, xs, ys, color="black")
+    push!(plt_l,plot!(plt_s1, xs, ys, color="black"))
 
-model = ridge.RidgeRegression(10.)
-ridge.fit(model, xx,yy)
-xs = [xmin, xmax]
-ys = [model.w_[1] + model.w_[2] * xmin,
-model.w_[1] + model.w_[2] * xmax]
+    model = ridge.RidgeRegression(10.)
+    ridge.fit(model, xx,yy)
+    xs = [xmin, xmax]
+    ys = [model.w_[1] + model.w_[2] * xmin,
+    model.w_[1] + model.w_[2] * xmax]
 
-plt3 = scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
-plt4 = plot!(plt3, xs, ys, color="black")
+    plt_s2 = scatter(xx, yy, color="black", xlim=(xmin, xmax), ylim=(ymin, ymax))
+    push!(plt_r, plot!(plt_s2, xs, ys, color="black"))
 
-plot(plt2, plt4, laout=(2,1))
+end
 
+plot(plt_l[1], plt_l[2], plt_l[3], plt_l[4], plt_l[5],
+     plt_r[1], plt_r[2], plt_r[3], plt_r[4], plt_r[5],
+     layout=(2,5))
