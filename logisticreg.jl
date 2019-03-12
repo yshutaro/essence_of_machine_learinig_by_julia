9ff75a03af1e52f9cf00ae54e698709141b83466
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-03-12 15:28:51 +0900

    05-06ロジスティック回帰 途中

--- logisticreg.jl
+++ logisticreg.jl
@@ -1,6 +1,9 @@
-module logisticRegression
+module logisticreg
 
-const THRESHMIN = 1e -10
+using LinearAlgebra
+using Random
+
+THRESHMIN = 1e-10
 
 sigmoid(x) = 1 / (1 + ℯ^(-1))
 
@@ -14,9 +17,27 @@ struct LogisticRegression
 end
 
 function fit(s::LogisticRegression, X, y)
+    s.w_ = randn(size(X)[1] + 1)
+    Xtil = hcat(ones(size(X)[1]), X)
+    diff = Inf
+    w_prev = s.w_
+    iter = 0
+    while diff > s.tol && iter < s.max_iter
+        yhat = sigmoid(dot(Xtil, s.w_))
+        r = clamp.(yhat .* (1 .- yhat), THRESHMIN, Inf)
+        XR = Xtil' * r
+        XRX = dot(Xtil' * r, Xtil)
+        w_prev = s.w_
+        b = dot(XR, dot(Xtil, s.w_) - 1 / r * (yhat - y))
+        s.w_ = XRX / b
+        diff = mean(abs(w_prev - s.w_))
+        iter = iter + 1
+    end
 end
 
 function predict(s::LogisticRegression, X)
+    Xtil = hcat(ones(size(X)[1]), X)
+    yhat = sigmoid(dot(Xtil, s.w_))
 end
 
 end
