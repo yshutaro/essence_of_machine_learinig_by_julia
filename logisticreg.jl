f40bf977cd43aebdb5ea5bebf1f158920fc04840
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-03-14 16:37:11 +0900

    途中

--- logisticreg.jl
+++ logisticreg.jl
@@ -6,19 +6,21 @@ using Statistics
 
 THRESHMIN = 1e-10
 
-sigmoid(x) = 1 / (1 + ℯ^(-x))
+sigmoid(x) = 1 ./ (1 .+ ℯ.^(-x))
 
 mutable struct LogisticRegression
     tol
     max_iter
+    random_seed
     w_
     function LogisticRegression(tol, max_iter=3, random_seed=0)
-        new(tol, max_iter, Nothing)
+        new(tol, max_iter, random_seed, Nothing)
     end
 end
 
 function fit(s::LogisticRegression, X, y)
-    s.w_ = randn(size(X)[1] + 1)
+    Random.seed!(s.random_seed)
+    s.w_ = randn(size(X)[2] + 1)
     println("size(s.w_):", size(s.w_))
     Xtil = hcat(ones(size(X)[1]), X)
     println("size(Xtil):", size(Xtil))
@@ -26,12 +28,23 @@ function fit(s::LogisticRegression, X, y)
     w_prev = s.w_
     iter = 0
     while diff > s.tol && iter < s.max_iter
-        yhat = sigmoid(dot(Xtil, s.w_))
+        yhat = sigmoid(Xtil * s.w_)
+        println("size(yhat):", size(yhat))
+        #r = clamp.(dot(yhat, (1 .- yhat)), THRESHMIN, Inf)
         r = clamp.(yhat .* (1 .- yhat), THRESHMIN, Inf)
+        println("r:", r)
+        println("size(r):", size(r))
+        println("size(Xtil'):", size(Xtil'))
         XR = Xtil' * r
-        XRX = dot(Xtil' * r, Xtil)
+        println("size(XR):", size(XR))
+        XRX = Xtil' * (r * Xtil)
         w_prev = s.w_
-        b = dot(XR, dot(Xtil, s.w_) - 1 / r * (yhat - y))
+        #println("Xtil * s.w_:", Xtil * s.w_)
+        println("1 / r :", 1 / r)
+        #println("yhat -y :", yhat -y)
+        println("size(Xtil * s.w_):", size(Xtil * s.w_))
+        println("size(yhat -y) :", size(yhat -y))
+        b = XR * ((Xtil * s.w_) - (1 / r) * (yhat - y))
         s.w_ = XRX / b
         diff = mean(abs(w_prev - s.w_))
         iter = iter + 1
