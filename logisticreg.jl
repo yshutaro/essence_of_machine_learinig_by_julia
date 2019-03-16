c6331a71e41fdb6578215dbd0759073ed2e75a85
yshutaro <y.shutaro@gmail.com>
2019-03-17 01:41:22 +0900

    05-06ロジスティック回帰　デバッグ用のprintlnを削除　完成

--- logisticreg.jl
+++ logisticreg.jl
@@ -26,44 +26,21 @@ function fit(s::LogisticRegression, X, y)
     w_prev = s.w_
     iter = 0
     while diff > s.tol && iter < s.max_iter
-        println("########$iter########")
         yhat = sigmoid(Xtil * s.w_)
-        println("(yhat):", yhat)
-        #println("size(yhat):", size(yhat))
         r = clamp.(yhat .* (1 .- yhat), THRESHMIN, Inf)
-        #println("r:", r)
-        #println("size(r):", size(r))
-        #println("size(Xtil'):", size(Xtil'))
         XR = Xtil' .* r'
-        #println("(XR):", XR)
-        println("size(XR):", size(XR))
         XRX = (Xtil' .* r') * Xtil
-        #println("(XRX):", XRX)
-        println("size(XRX):", size(XRX))
         w_prev = s.w_
-        #println("size(Xtil * s.w_):", size(Xtil * s.w_))
-        #println("size(yhat -y) :", size(yhat -y))
-        #println("size(1 ./ r) :", size(1 ./ r))
-        #println("size(s.w_) :", size(s.w_))
-        #println("size(Xtil * s.w_) :", size(Xtil * s.w_))
-        #println("size dot((1 ./ r) , (yhat - y)):",size( dot((1 ./ r), (yhat - y) )))
-        #println("size( (Xtil * s.w_) - (1 ./ r) .* (yhat - y) ):",size( (Xtil * s.w_) .- (1 ./ r) .* (yhat - y) ))
         b = XR * (Xtil * s.w_ .- (1 ./ r) .* (yhat - y)) 
-        #println("size(b):", size(b))
         s.w_ = XRX \ b
-        println("#####s.w_#####", s.w_)
         diff = mean(abs.(w_prev - s.w_))
         iter = iter + 1
     end
 end
 
 function predict(s::LogisticRegression, X)
-    println("###predict###")
     Xtil = hcat(ones(size(X)[1]), X)
-    #println("Xtil::", Xtil)
-    #println("Xtil * s.w_::", Xtil * s.w_)
     yhat = sigmoid(Xtil * s.w_)
-    #println("yhat::", yhat)
     return [ifelse(x .> .5, 1, 0) for x in yhat]
 end
 
