9890bed4d8e30fff955b543269cdc57b00c0fae1
yshutaro <y.shutaro@gmail.com>
2019-03-17 00:23:33 +0900

    05-06ロジスティック回帰　ほぼ完成

--- logisticreg_wdbc.jl
+++ logisticreg_wdbc.jl
@@ -20,17 +20,19 @@ X_train = X[1:row-n_test, :]
 y_test = y[end-n_test+1:end]
 X_test = X[end-n_test+1:end, :]
 
-println("size(y):", size(y))
-println("size(y_train):", size(y_train))
-println("size(y_test):", size(y_test))
-println("size(X):", size(X))
-println("size(X_train):", size(X_train))
-println("size(X_test):",size(X_test))
+#println("size(y):", size(y))
+#println("size(y_train):", size(y_train))
+#println("size(y_test):", size(y_test))
+#println("size(X):", size(X))
+#println("size(X_train):", size(X_train))
+#println("size(X_test):",size(X_test))
 
 model = logisticreg.LogisticRegression(0.01)
 logisticreg.fit(model, X_train, y_train)
 
 y_predict = logisticreg.predict(model, X_test)
+println(y_test)
+println(y_predict)
 n_hits = sum(y_test == y_predict)
 a = 0
 for i in 1:length(y_test)
