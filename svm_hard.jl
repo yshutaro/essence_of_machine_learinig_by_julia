9864cef42044d4d8252663f7983c34ba76f80c86
yshutaro <y.shutaro@gmail.com>
2019-03-24 04:28:52 +0900

    ydfのインデックスを正しく取得できた。

--- svm_hard.jl
+++ svm_hard.jl
@@ -2,7 +2,7 @@ module svm_hard
 #using LinearAlgebra
 
 mutable struct SVC
-    a
+    a_
     w_
     w0_
     function SVC()
@@ -17,29 +17,41 @@ function fit(s::SVC, X, y, selections=Nothing)
     ay = 0
     ayx = zeros(size(X)[2])
     yx = y .* X
-    indices = collect(1:size(X)[1])
+    #indices = collect(1:size(X)[1])
+    count = 0
     while true
-        println("size(yx):", size(yx))
-        println("size(ayx):", size(ayx))
+        println("##### ", count, " ######")
+        #println("size(yx):", size(yx))
+        #println("size(ayx):", size(ayx))
+        println("1 . -(yx * ayx):",1 .- (yx * ayx))
         ydf = y .* (1 .- (yx * ayx))
-        println("size(ydf):", size(ydf))
-        println("size(indices):", size(indices))
-        iydf = hcat(indices, ydf)
-        println("size(iydf):", size(iydf))
-        #println(size((y .< 0) .| (a .> 0)))
-        println(iydf)
-        println((y .< 0) .| (a .> 0))
-        println(iydf[:, 1][(y .< 0) .| (a .> 0)])
-        println(size(iydf[:, 1][(y .< 0) .| (a .> 0)]))
-        println("#####")
-        i = minimum(iydf[:, 1][(y .< 0) .| (a .> 0)])
-        j = maximum(iydf[:, 1][(y .< 0) .| (a .> 0)])
+        println("ydf:", ydf)
+        #println("size(ydf):", size(ydf))
+        #iydf = hcat(indices, ydf)
+        #println("iydf  :",iydf)
+        #println(size(iydf[:, 1][(y .< 0) .| (a .> 0)]))
+        #println("#####")
+        #i = minimum(iydf[:, 1][(y .< 0) .| (a .> 0)])
+        #j = maximum(iydf[:, 1][(y .< 0) .| (a .> 0)])
+        #println(iydf[(y .< 0) .| (a .> 0)])
+        i = findfirst(ydf .== minimum(ydf[(y .< 0) .| (a .> 0)]))
+        println("i  :",i)
+        j = findfirst(ydf .== maximum(ydf[(y .> 0) .| (a .> 0)]))
+        println("j  :",j)
+        println("y:", y)
+        println("y[i]:", y[i])
+        println("y[j]:", y[j])
+        println("a[i]:", a[i])
+        println("a[j]:", a[j])
         if ydf[i] >= ydf[j]
             break
         end
         ay2 = ay - y[i]*a[i] - y[j]*a[j]
-        ayx2 = ayx .- y[i]*a[i]*X[i, :] .- y[j]*a[j]*X[j, :]
-        ai = (1 - y[i]*y[j] + y[i]* ( (X[i, :] - X[j, :]) * (X[j, :] * ay2 - ayx2) ) ) / sum((X[i] .- X[j]).^2)
+        println("ay2 :", ay2)
+        ayx2 = ayx .- y[i]*a[i].*X[i, :] .- y[j]*a[j].*X[j, :]
+        println("ayx2 :", ayx2)
+        ai = (1 - y[i]*y[j] .+ y[i] .* ( (X[i, :] .- X[j, :]) .* (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i] .- X[j]).^2)
+        println("ai :", ai)
         ai = (ai < 0 ? 0 : ai)
         aj = (-ai * y[i] - ay2) * y[j]
         if aj < 0
@@ -54,9 +66,20 @@ function fit(s::SVC, X, y, selections=Nothing)
         end
         a[i] = ai
         a[j] = aj
+        count += 0
     end
-    s.a = a
-    s.w_ = sum(reshape(a[ind] * y[ind], 1, :) .* X[ind, :])
+    s.a_ = a
+    ind = a .!= 0.
+    println("ind:", ind)
+    #s.w_ = sum(reshape(a[ind] .* y[ind], 1, :) .* X[ind, :])
+    println("size(X[ind, :]):", size(X[ind, :]))
+    println("X[ind, :]:", X[ind, :])
+    println("a:", a)
+    println("a[ind]:", a[ind])
+    println("y[ind]:", y[ind])
+    s.w_ = sum((a[ind] .* y[ind]) .* X[ind, :])
+    println("s.w_:", s.w_)
+    println("size(s.w_):", size(s.w_))
     s.w0_ = sum(y[ind] - (X[ind, :] * s.w_)) / sum(ind)
 end
 
