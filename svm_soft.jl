1d5c7bab8150803bb9fc97765206b1714661d817
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-03-25 16:32:12 +0900

    05-07サポートベクタマシン 分類不可能な場合　途中

---
+++ svm_soft.jl
@@ -0,0 +1,63 @@
+module svm_soft
+using LinearAlgebra
+
+mutable struct SVC
+    a_
+    w_
+    w0_
+    C
+    function SVC(C=1.)
+        new(Nothing, Nothing, Nothing, C)
+    end
+end
+
+function fit(s::SVC, X, y, selections=Nothing)
+    a = zeros(size(X)[1])
+    ay = 0
+    ayx = zeros(size(X)[2])
+    yx = y .* X
+    while true
+        ydf = y .* (1 .- (yx * ayx))
+        i = findfirst(ydf .== minimum(ydf[((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0))]))
+        j = findfirst(ydf .== maximum(ydf[((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0))]))
+        if ydf[i] >= ydf[j]
+            break
+        end
+        ay2 = ay - y[i]*a[i] - y[j]*a[j]
+        ayx2 = ayx .- y[i]*a[i].*X[i, :] .- y[j]*a[j].*X[j, :]
+        ai = (1 - y[i]*y[j] .+ y[i] .* dot( (X[i, :] .- X[j, :]) , (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i] - X[j])^2)
+        ai = (ai < 0 ? 0 : ai)
+        if ai < 0
+            ai = 0
+        elseif ai > s.C
+            ai = s.C
+        end
+        aj = (-ai * y[i] - ay2) * y[j]
+        if aj < 0
+            aj = 0
+            ai = (-aj * y[j] - ay2) * y[i]
+        elseif aj > s.C
+            aj = s.C
+            ai = (-aj * y[j] - ay2) * y[i]
+        end
+
+        ay = ay + y[i] * (ai - a[i]) + y[j] * (aj -a[j])
+        ayx = ayx .+ y[i] * (ai - a[i]) .* X[i, :] + y[j] * (aj -a[j]) .* X[j, :]
+        println("a[i]: ", a[i])
+        if ai == a[i]
+            break
+        end
+        a[i] = ai
+        a[j] = aj
+    end
+    s.a_ = a
+    ind = a .!= 0.
+    s.w_ = sum((a[ind] .* y[ind]) .* X[ind, :], dims=1)
+    s.w0_ = sum(y[ind] .- (X[ind, :] * (s.w_)')) / sum(ind)
+end
+
+function predict(s::SVC, X)
+    sign.(s.w0_ .+ X * (s.w_)')
+end
+
+end
---
+++ svm_soft_test1.jl
@@ -0,0 +1,28 @@
+include("svm_soft.jl")
+
+using .svm_soft
+using Plots
+using Random
+
+Random.seed!(0)
+X0 = randn(20, 2)
+X1 = randn(20, 2) .+ [2.5 3]
+y = vcat([1 for x in 1:20], [-1 for x in 1:20])
+
+X = vcat(X0, X1)
+
+model = svm_soft.SVC()
+svm_soft.fit(model, X, y)
+
+scatter(X0[:, 1], X0[:, 2], color="black", markershape=:+, label="")
+scatter!(X1[:, 1], X1[:, 2], color="black", markershape=:star6, label="")
+
+f(model, x) = (-model.w0_ - model.w_[1] * x) / model.w_[2]
+
+x1 = -2
+x2 = 4
+plot!([x1, x2], [f(model, x1), f(model, x2)], color="black", label="")
+tf = model.a_ .!= 0
+Xfalse = [false for x in 1:size(X)[1]]
+print("正しく分類できた数:", sum(svm_soft.predict(model, X) .== y))
+scatter!(X[hcat(tf, Xfalse)], X[hcat(Xfalse, tf)], color="red", markersize=10, markershape=:circle, markeralpha=0.1, label="")
