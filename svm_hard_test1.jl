86892cdc4f6eba3722b2d2bd02a2cc983d3af574
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-03-25 13:58:56 +0900

    表示までできたのでコミット。分割がおかしいので修正予定。

--- svm_hard_test1.jl
+++ svm_hard_test1.jl
@@ -26,5 +26,12 @@ f(model, x) = (-model.w0_ - model.w_[1] * x) / model.w_[2]
 
 x1 = -0.2
 x2 = 6
+#println("size(a_): ", size(model.a_))
+println("[model.a_ .!= 0] :", [model.a_ .!= 0])
+println("X: ", X)
+println("size(X): ", size(X))
 plot!([x1, x2], [f(model, x1), f(model, x2)], color="black")
-#scatter!(X[model.a_ != 0, 0], X[model.a_ != 0, 1])
+tf = model.a_ .!= 0
+Xfalse = [false for x in 1:size(X)[1]]
+println(X[hcat(tf, Xfalse)])
+scatter!(X[hcat(tf, Xfalse)], X[hcat(Xfalse, tf)], color="red", markersize=10, markershape=:circle, markeralpha=0.1)
