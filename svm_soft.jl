301d2af643038f583d041f409e4c39c0c832c6ae
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-03-26 16:57:51 +0900

    05-07サポートベクタマシン　hard, softデバッグ中

--- svm_soft.jl
+++ svm_soft.jl
@@ -16,23 +16,32 @@ function fit(s::SVC, X, y, selections=Nothing)
     ay = 0
     ayx = zeros(size(X)[2])
     yx = y .* X
+    count=0
     while true
+        println("###### ", count, " #####")
         ydf = y .* (1 .- (yx * ayx))
         i = findfirst(ydf .== minimum(ydf[((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0))]))
+        println("i :", i)
         j = findfirst(ydf .== maximum(ydf[((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0))]))
+        println("j :", j)
         if ydf[i] >= ydf[j]
             break
         end
         ay2 = ay - y[i]*a[i] - y[j]*a[j]
+        println("ay2 :", ay2)
         ayx2 = ayx .- y[i]*a[i].*X[i, :] .- y[j]*a[j].*X[j, :]
-        ai = (1 - y[i]*y[j] .+ y[i] .* dot( (X[i, :] .- X[j, :]) , (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i] - X[j])^2)
-        ai = (ai < 0 ? 0 : ai)
+        println("ayx2 :", ayx2)
+        ai = (1 - y[i]*y[j] .+ y[i] .* dot( (X[i, :] .- X[j, :]) , (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i, :] .- X[j, :]).^2)
+        println("ai :", ai)
         if ai < 0
             ai = 0
         elseif ai > s.C
             ai = s.C
         end
+        println("ai :", ai)
+
         aj = (-ai * y[i] - ay2) * y[j]
+        println("aj 1 :", aj)
         if aj < 0
             aj = 0
             ai = (-aj * y[j] - ay2) * y[i]
@@ -40,15 +49,21 @@ function fit(s::SVC, X, y, selections=Nothing)
             aj = s.C
             ai = (-aj * y[j] - ay2) * y[i]
         end
+        println("aj :", aj)
 
         ay = ay + y[i] * (ai - a[i]) + y[j] * (aj -a[j])
+        println("ay: ", ay)
         ayx = ayx .+ y[i] * (ai - a[i]) .* X[i, :] + y[j] * (aj -a[j]) .* X[j, :]
-        println("a[i]: ", a[i])
+        println("ayx: ", ayx)
         if ai == a[i]
             break
         end
         a[i] = ai
         a[j] = aj
+        count += 1
+        #if count == 2
+        #    break
+        #end
     end
     s.a_ = a
     ind = a .!= 0.
