b61f294be15ea9aea3f95174d67da24c6ae490bf
yshutaro <y.shutaro@gmail.com>
2019-03-28 22:31:00 +0900

    05-07サポートベクタマシン　hard　デバッグ中

--- svm_soft.jl
+++ svm_soft.jl
@@ -18,41 +18,71 @@ function fit(s::SVC, X, y, selections=Nothing)
     yx = y .* X
     count=0
     while true
-        println("###### ", count, " #####")
+        println("############### ", count, " ##############")
+        #println("yx :", yx)
+        #println("ayx :", ayx)
         ydf = y .* (1 .- (yx * ayx))
+        println("ydf :", ydf)
+        #println("((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0)) :", ((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0)))
+        println("find all i", findfirst(ydf .== minimum(ydf[((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0))])))
         i = findfirst(ydf .== minimum(ydf[((a .> 0) .& (y .> 0)) .| ((a .< s.C) .& (y .< 0))]))
         println("i :", i)
+        println("ydf[i] :", ydf[i])
+        #println("((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0)) :", ((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0)))
+        println("ydf true : ", ydf[((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0))])
+        println("ydf[18] : ", ydf[19])
+        println("ydf[25] : ", ydf[25])
+        println("find all j",findfirst(ydf .== maximum(ydf[((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0))])))
         j = findfirst(ydf .== maximum(ydf[((a .> 0) .& (y .< 0)) .| ((a .< s.C) .& (y .> 0))]))
         println("j :", j)
+        println("ydf[j] :", ydf[j])
         if ydf[i] >= ydf[j]
             break
         end
+        println("ay :", ay)
+        println("y[i] :", y[i])
+        println("a[i] :", a[i])
+        println("y[j] :", y[j])
+        println("a[j] :", a[j])
         ay2 = ay - y[i]*a[i] - y[j]*a[j]
         println("ay2 :", ay2)
         ayx2 = ayx .- y[i]*a[i].*X[i, :] .- y[j]*a[j].*X[j, :]
         println("ayx2 :", ayx2)
-        ai = (1 - y[i]*y[j] .+ y[i] .* dot( (X[i, :] .- X[j, :]) , (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i, :] .- X[j, :]).^2)
-        println("ai :", ai)
+        ai = (1 - y[i]*y[j] + y[i] * dot( (X[i, :] .- X[j, :]) , (X[j, :] .* ay2 .- ayx2) ) ) / sum((X[i, :] .- X[j, :]).^2)
+        #println("X[i, :] :", X[i, :])
+        #println("X[j, :] :", X[j, :])
+        #println("sum((X[i, :] .- X[j, :]).^2): ", sum((X[i, :] .- X[j, :]).^2))
+        println("ai 1:", ai)
         if ai < 0
+            println("############ai<0#######")
             ai = 0
         elseif ai > s.C
+            println("############ai>s.C#######")
             ai = s.C
         end
-        println("ai :", ai)
+        println("ai 2:", ai)
 
         aj = (-ai * y[i] - ay2) * y[j]
-        println("aj 1 :", aj)
+        println("aj 1:", aj)
         if aj < 0
+            println("############aj<0#######")
             aj = 0
             ai = (-aj * y[j] - ay2) * y[i]
         elseif aj > s.C
+            println("############aj>s.C#######")
             aj = s.C
             ai = (-aj * y[j] - ay2) * y[i]
         end
-        println("aj :", aj)
+        println("ai 3 :", ai)
+        println("aj 2:", aj)
 
+        println("ay before:", ay)
+        println("y[i] :", y[i])
+        println("a[i] :", a[i])
+        println("a[j] :", a[j])
         ay = ay + y[i] * (ai - a[i]) + y[j] * (aj -a[j])
         println("ay: ", ay)
+        println("ai - a[i]: ", ai - a[i])
         ayx = ayx .+ y[i] * (ai - a[i]) .* X[i, :] + y[j] * (aj -a[j]) .* X[j, :]
         println("ayx: ", ayx)
         if ai == a[i]
@@ -61,9 +91,9 @@ function fit(s::SVC, X, y, selections=Nothing)
         a[i] = ai
         a[j] = aj
         count += 1
-        #if count == 2
-        #    break
-        #end
+        if count > 100
+            break
+        end
     end
     s.a_ = a
     ind = a .!= 0.
