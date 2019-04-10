335e8c4b6b869f1b112a4a4aca0a689900cab2d2
shukenro <yshutaro@gmail.com>
2019-04-10 12:08:38 +0900

    Kmeans実装途中

--- kmeans.jl
+++ kmeans.jl
@@ -6,17 +6,35 @@ mutable struct KMeans
     n_clusters
     max_iter::Int
     random_seed
+    labels_
     cluster_centers_
     function KMeans(n_clusters)
-        new(n_clusters, 1000, 0, Nothing)
+        new(n_clusters, 1000, 0, Nothing, Nothing)
     end
 end
 
 function fit(obj::KMeans, X)
     Random.seed!(obj.random_seed)
 
+    labels_prev = zeros(size(X)[1])
     count = 0
     obj.cluster_centers_ = zeros(obj.n_clusters, size(X)[2])
+    while (!(obj.labels_ .== labels_prev) && count < obj.max_iter)
+        for i in 1:obj.n_clusters
+            XX = X[obj.labels_ .== i, :]
+        end
+        # TODO
+        dist = []
+        labels_prev = obj.labels_
+        obj.labels_ = argmin(dist)
+        count += 1
+    end
+end
+
+function predict(obj::KMeans, X)
+    # TODO
+    dist = []
+    labels = argmin(dist)
 end
 
 end
