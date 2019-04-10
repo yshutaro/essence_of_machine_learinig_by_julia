d04f88f9f7fdfc80def0b6113aad10914e70900b
yshutaro <44796318+ShutaroYoshizumiNTTPC@users.noreply.github.com>
2019-04-10 11:42:12 +0900

    KMeans実装途中

--- kmeans.jl
+++ kmeans.jl
@@ -1,16 +1,22 @@
 module kmeans
 
+using Random
+
 mutable struct KMeans
     n_clusters
     max_iter::Int
     random_seed
+    cluster_centers_
     function KMeans(n_clusters)
-        new(n_clusters, 1000, 0)
+        new(n_clusters, 1000, 0, Nothing)
     end
 end
 
-function fit()
-    
+function fit(obj::KMeans, X)
+    Random.seed!(obj.random_seed)
+
+    count = 0
+    obj.cluster_centers_ = zeros(obj.n_clusters, size(X)[2])
 end
 
 end
