28efce84ce9085fef33079145e46a38c91739329
yshutaro <y.shutaro@gmail.com>
2019-04-14 01:03:15 +0900

    05-08k-Means法 KMeans実装一旦エラー無し

--- kmeans.jl
+++ kmeans.jl
@@ -1,21 +1,24 @@
 module kmeans
 
 using Random
+using Base.Iterators
 
 mutable struct KMeans
     n_clusters
     max_iter::Int
-    random_seed
+    rng_::MersenneTwister
     labels_
     cluster_centers_
     function KMeans(n_clusters)
-        new(n_clusters, 1000, 0, Nothing, Nothing)
+        rng = MersenneTwister(0)
+        new(n_clusters, 1000, rng, Nothing, Nothing)
     end
 end
 
 function fit(obj::KMeans, X)
-    Random.seed!(obj.random_seed)
-
+    cycle = Iterators.cycle(collect(0:obj.n_clusters))
+    obj.labels_ = collect(Iterators.take(cycle,size(X)[1]))
+    shuffle(obj.rng, obj.labels_)
     labels_prev = zeros(size(X)[1])
     count = 0
     obj.cluster_centers_ = zeros(obj.n_clusters, size(X)[2])
@@ -23,8 +26,8 @@ function fit(obj::KMeans, X)
         for i in 1:obj.n_clusters
             XX = X[obj.labels_ .== i, :]
         end
-        # TODO
-        dist = []
+        dist = [sum((X[i, :] .- obj.cluster_centers[j, :]).^2)
+         for i in 1:size(X)[1], j in 1:size(obj.cluster_centers)[1]]
         labels_prev = obj.labels_
         obj.labels_ = argmin(dist)
         count += 1
@@ -32,8 +35,8 @@ function fit(obj::KMeans, X)
 end
 
 function predict(obj::KMeans, X)
-    # TODO
-    dist = []
+    dist = [sum((X[i, :] .- obj.cluster_centers[j, :]).^2)
+     for i in 1:size(X)[1], j in 1:size(obj.cluster_centers)[1]]
     labels = argmin(dist)
 end
 
