d986bec945d7ff9960dfd896826c51a030bc030d
yshutaro <yshutaro@gmail.com>
2019-04-14 23:45:09 +0900

    05-08k-Means法　最終版

--- kmeans.jl
+++ kmeans.jl
@@ -17,29 +17,21 @@ mutable struct KMeans
 end
 
 function fit(obj::KMeans, X)
-    println("size X : ", size(X))
     cycle = Iterators.cycle(collect(1:obj.n_clusters))
     obj.labels_ = collect(Iterators.take(cycle,size(X)[1]))
-    println("### obj.labels_ :", (obj.labels_))
     shuffle!(obj.rng_, obj.labels_)
-    println("size obje.labels_ : ", size(obj.labels_))
     labels_prev = zeros(size(X)[1])
     count = 0
     obj.cluster_centers_ = zeros(obj.n_clusters, size(X)[2])
     while (!all(obj.labels_ .== labels_prev) && count < obj.max_iter)
-        println("### ", count, " ###")
         for i in 1:obj.n_clusters
-            println("### i = ", i, " ###")
             XX = X[obj.labels_ .== i, :]
             obj.cluster_centers_[i, :] = mean(XX, dims=1)
         end
-        println("obj.cluster_centers_ : ", obj.cluster_centers_)
         dist = [sum((X[i, :] .- obj.cluster_centers_[j, :]).^2)
          for i in 1:size(X)[1], j in 1:size(obj.cluster_centers_)[1]]
         labels_prev = obj.labels_
-        println("size dist : ", size(dist))
         obj.labels_ = vec([x[2] for x in argmin(dist, dims=2)])
-        println("obj.labels_ : ", obj.labels_)
         count += 1
     end
 end
