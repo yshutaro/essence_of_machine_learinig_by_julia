ffa00764a6102a52d2a756660b55830a5b8c0810
yshutaro <yshutaro@gmail.com>
2019-04-16 22:13:24 +0900

    pca_test初回

---
+++ pca_test1.jl
@@ -0,0 +1,18 @@
+include("./pca.jl")
+using .pca
+using CSV
+using Plots
+
+dataframe = CSV.read("winequality-red.csv", header=true, delim=';')
+row,col=size(dataframe)
+
+Xy = Float64[dataframe[r,c] for r in 1:row, c in 1:col]
+
+X = Xy[:, end-1]
+
+model = pca.PCA(2)
+pca.fit(model, X)
+
+Y = pca.transform(X)
+
+scatter(Y[:,1], Y[:,2], color=:black)
