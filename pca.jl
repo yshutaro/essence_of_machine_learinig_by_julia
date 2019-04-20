b4f906b6b50062099d9c85ee9dc539725478f280
yshutaro <yshutaro@gmail.com>
2019-04-21 03:07:15 +0900

    PCA 一旦動作確認

--- pca.jl
+++ pca.jl
@@ -3,6 +3,8 @@ module pca
 using Random
 using Statistics
 using LinearAlgebra
+using PyCall
+@pyimport scipy.sparse.linalg as linalg
 
 mutable struct PCA
     n_components
@@ -22,9 +24,16 @@ function fit(obj::PCA, X)
     println("xbar:", xbar)
     Y = X .- xbar
     S = Y' * Y
-    U, Σ, VT = svd(S)
+    U, Σ, VT = linalg.svds(S, k=obj.n_components, tol=obj.tol)
+    println("U size:",size(U))
     println("VT size:",size(VT))
-    obj.VT_ = VT
+    println("Σ size:",size(Σ))
+    println("U :",(U))
+    println("Σ:",Σ)
+    println("VT :",(VT))
+    println("reverse VT :", VT[end:-1:1, :])
+    println("reverse VT size:", size(VT[end:-1:1, :]))
+    obj.VT_ = VT[end:-1:1, :]
 end
 
 function transform(obj::PCA, X)
